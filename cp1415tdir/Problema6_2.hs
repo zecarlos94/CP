ca4ae50f018355fc6f3fd8e9362553133065c1bd
zecarlos94 <zecarlos@MBP-de-Jose-4.home>
2015-05-24 18:02:29 +0100

    alterei

--- cp1415tdir/Problema6_2.hs
+++ cp1415tdir/Problema6_2.hs
@@ -3,21 +3,22 @@ module Problema6_2 where
 import List
 import Probability
 
-{-
+
 
 -- Exemplo fornecido no enunciado
 
-pcataList :: (Either () (a, b) -> Dist b) -> [a ] -> Dist b
-
 padd (a, b) = D [(a + b, 0.9), (a − b, 0.1)]
+pcataList :: (Either () (a, b) -> Dist b) -> [a] -> Dist b
 
 d4 = pcataList [pzero, padd] [20, 10, 5] where pzero = return · zero
 
--}
+--pcataList (either (return.zero) padd) [20,10,5]
 
+
+{-
 pcataList :: (Either () (a, b) -> Dist b) -> [a] -> Dist b
 
-pwordAux a x = D [((a==x && a\="Stop"), 0.05), (a=="Stop", 0.1) , (a\=x,0.85)]
+pwordAux a x = D [((a==x && a/="Stop"), 0.05), (a=="Stop", 0.1) , (a/=x,0.85)]
 			
 pwords [a] x = (pwords (tail a) x).(pwordAux a x) 
 
@@ -32,3 +33,8 @@ test07 = gene (i2 ("a", ["b"]))
 
 --Resultado a obter 
 transmitir (words "Vamos atacar hoje")
+
+-}
+
+--pcataList (either (return.zero) pwords) (words "Vamos atacar hoje")
+
